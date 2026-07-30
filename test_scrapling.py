7e3eda1d4bef46aa700226eef4e9e3c3454174f5
jpxprojeeect <109829830+jpxprojeeect@users.noreply.github.com>
2026-07-30 10:28:07 +0700

    chore: cleanup - remove duplicate and unnecessary files

--- test_scrapling.py
+++
@@ -1,8 +0,0 @@
-from scrapling.fetchers import Fetcher
-
-# HTTP request biasa
-page = Fetcher.get('https://quotes.toscrape.com/')
-quotes = page.css('.quote .text::text').getall()
-
-for q in quotes[:3]:
-    print(q)
